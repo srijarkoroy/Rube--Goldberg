52332f73caa4c8ed87c4d94639c9a8089638c1a0
Indira Dutta <66861242+indiradutta@users.noreply.github.com>
2020-11-18 20:54:18 +0530

    Add files via upload

--- driver.cpp
+++ driver.cpp
@@ -89,6 +89,219 @@ public:
     }
 };
 
+//creating a stack
+class Stack
+{
+private:
+    struct Node *head;
+
+public:
+    Stack()
+    {
+        head = NULL;
+    }
+    //inserting elements in the stack
+    void Insertion(char *name, int Age, int dob)
+    {
+        struct Node *temp = new Node(name, Age, dob);
+        if (head == NULL)
+        {
+            head = temp;
+        }
+        else
+        {
+            temp->next = head;
+            head = temp;
+        }
+    }
+    //popping elements from the stack
+    struct Wrapper Pop()
+    {
+        if (head != NULL)
+        {
+            struct Node *temp = head;
+            head = temp->next;
+            struct Wrapper p1;
+            strcpy(p1.Name, temp->Name);
+            p1.age = temp->age;
+            p1.DOB = temp->DOB;
+            delete temp;
+            return p1;
+        }
+    }
+    //printing elements of the stack
+    void Print()
+    {
+        struct Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->Name << " | " << temp->age << " | " << temp->DOB << endl;
+            temp = temp->next;
+        }
+        return;
+    }
+};
+
+//creating a tree
+class Tree
+{
+private:
+    vector<struct Wrapper> v1;
+
+public:
+    //inserting elements in the tree
+    void Insertion(char *name, int Age, int dob)
+    {
+        struct Wrapper p;
+        strcpy(p.Name, name);
+        p.age = Age;
+        p.DOB = dob;
+        v1.push_back(p);
+    }
+    int getLeft(int i)
+    {
+        return ((2 * i) + 1);
+    }
+    int getRight(int i)
+    {
+        return ((2 * i) + 2);
+    }
+    //inorder traversal in the tree
+    void Inorder(int a, vector<struct Wrapper> &ans)
+    {
+        if (a >= v1.size())
+        {
+            return;
+        }
+        Inorder(getLeft(a), ans);
+        ans.push_back(v1[a]);
+        Inorder(getRight(a), ans);
+    }
+    //pre order traversal in the tree
+    void preOrder(int a)
+    {
+        if (a >= v1.size())
+        {
+            return;
+        }
+        cout << v1[a].Name << " | " << v1[a].age << " | " << v1[a].DOB << endl;
+        preOrder(getLeft(a));
+        preOrder(getRight(a));
+    }
+    //post order traversal in the tree
+    void postOrder(int a)
+    {
+        if (a >= v1.size())
+        {
+            return;
+        }
+        postOrder(getLeft(a));
+        postOrder(getRight(a));
+        cout << v1[a].Name << " | " << v1[a].age << " | " << v1[a].DOB << endl;
+    }
+};
+//creating a linked list
+class List
+{
+private:
+    struct Node *head;
+
+public:
+    List()
+    {
+        head = NULL;
+    }
+    //inserting elements in the linked list
+    void Insertion(char *name, int Age, int dob)
+    {
+        struct Node *temp = new Node(name, Age, dob);
+        if (head == NULL)
+        {
+            head = temp;
+        }
+        else
+        {
+            struct Node *temp2 = head;
+            while (temp2->next != NULL)
+            {
+                temp2 = temp2->next;
+            }
+            temp2->next = temp;
+            temp->prev = temp2;
+        }
+    }
+    //swapping two nodes of the linked list
+    void swap(Node *c, Node *d)
+    {
+        char tempL[30];
+        char tempR[30];
+        int tempAge;
+        int tempDOB;
+        strcpy(tempR, c->Name);
+        tempAge = c->age;
+        tempDOB = c->DOB;
+        strcpy(c->Name, d->Name);
+        c->age = d->age;
+        c->DOB = d->DOB;
+        strcpy(d->Name, tempR);
+        d->age = tempAge;
+        d->DOB = tempDOB;
+    }
+    //printing all the elements of th linked list
+    void Print()
+    {
+        struct Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->Name << " | " << temp->age << " | " << temp->DOB << endl;
+            temp = temp->next;
+        }
+    }
+    //getting the last node of the linked list
+    Node *lastNode()
+    {
+        struct Node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        return temp;
+    }
+    //partitioning the list for quick sort
+    Node *Partition(struct Node *first, struct Node *last)
+    {
+        int pValue = last->age;
+        Node *i = first->prev;
+        for (Node *j = first; j != last; j = j->next)
+        {
+            if (j->age <= pValue)
+            {
+                i = (i == NULL) ? first : i->next;
+                swap(i, j);
+            }
+        }
+        i = (i == NULL) ? first : i->next;
+        swap(i, last);
+        return i;
+    }
+    //sorting the linked list using quick sort
+    void quickSort(struct Node *first, struct Node *tail)
+    {
+        if (tail != NULL && first != tail && first != tail->next)
+        {
+            Node *pindex = Partition(first, tail);
+            quickSort(first, pindex->prev);
+            quickSort(pindex->next, tail);
+        }
+    }
+    void Sort()
+    {
+        struct Node *tail = lastNode();
+        struct Node *first = head;
+        quickSort(first, tail);
+    }
+};
+
 int main()
 {
     int n = 1;
